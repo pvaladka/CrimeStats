5e5ee8aaeed22688acca50a8a57f5cb5bd4819bf
pvaladka <patrickvaladka@gmail.com>
2017-09-26 15:04:17 -0500

    the best commitment

---
+++ kitchen_reorganization.sh
@@ -0,0 +1,11 @@
+mv ./kitchen/cans.txt ./kitchen/pantry/
+rm ./kitchen/fridge/diapers.txt
+rm ./kitchen/fridge/freezer/couch.txt
+mv ./kitchen/fridge/trashcan/ ./kitchen
+mv ./kitchen/trashcan/sink/ ./kitchen
+rm ./kitchen/sink/delete-me.txt
+mkdir ./kitchen/sink/drying-rack
+mv ./kitchen/sink/clean-dishes.txt ./kitchen/sink/drying-rack/
+mv ./kitchen/pantry/crisper-drawer/ ./kitchen/fridge/
+touch ./kitchen/fridge/crisper-drawer/apples.txt
+
